9ac228b680d00469cbb4811707f29ad87d6a4fd4
Conor Knowles <knoconor@amazon.com>
2024-10-10 13:43:41 +0000

    Include fuzzy-lib in build artifacts

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "Set of simple, performant neovim plugins";
+  description = "Set of simple, performant Neovim plugins";
 
   inputs = {
     nixpkgs.url = "github:cachix/devenv-nixpkgs/rolling";
@@ -15,73 +15,75 @@
     extra-substituters = "https://devenv.cachix.org";
   };
 
-  outputs = inputs@{ flake-parts, nixpkgs, fenix, ... }:
-    flake-parts.lib.mkFlake { inherit inputs; } {
-      imports = [ inputs.devenv.flakeModule ];
-      systems = [
-        "x86_64-linux"
-        "i686-linux"
-        "x86_64-darwin"
-        "aarch64-linux"
-        "aarch64-darwin"
-      ];
+  outputs = inputs@{ flake-parts, nixpkgs, fenix, ... }: flake-parts.lib.mkFlake { inherit inputs; } {
+    imports = [ inputs.devenv.flakeModule ];
+    systems = [
+      "x86_64-linux"
+      "i686-linux"
+      "x86_64-darwin"
+      "aarch64-linux"
+      "aarch64-darwin"
+    ];
 
-      perSystem = { config, self', inputs', pkgs, system, lib, ... }: {
-        # define the packages provided by this flake
-        packages = let
-          inherit (fenix.packages.${system}.minimal) toolchain;
+    perSystem = { config, self', inputs', pkgs, system, lib, ... }: {
+      # define the packages provided by this flake
+      packages = let
+        inherit (fenix.packages.${system}.minimal) toolchain;
 
-          rustPlatform = pkgs.makeRustPlatform {
-            cargo = toolchain;
-            rustc = toolchain;
-          };
-
-          src = ./.;
-          version = "2024-08-02";
-
-          blink-fuzzy-lib = rustPlatform.buildRustPackage {
-            pname = "blink-fuzzy-lib";
-            inherit src version;
-            cargoLock = {
-              lockFile = ./Cargo.lock;
-              outputHashes = {
-                "c-marshalling-0.2.0" =
-                  "sha256-eL6nkZOtuLLQ0r31X7uroUUDYZsWOJ9KNXl4NCVNRuw=";
-                "frizbee-0.1.0" =
-                  "sha256-9L3ZS7GMvLEqOBjC/VW2jHEZge/s6jRN7ok647Frl/M=";
-              };
-            };
-          };
-        in {
-          blink-cmp = pkgs.vimUtils.buildVimPlugin {
-            pname = "blink-cmp";
-            inherit src version;
-            preInstall = ''
-              mkdir -p target/release
-              ln -s ${blink-fuzzy-lib}/lib/libblink_cmp_fuzzy.so target/release/libblink_cmp_fuzzy.so
-            '';
-
-            meta = {
-              description =
-                "Performant, batteries-included completion plugin for Neovim ";
-              homepage = "https://github.com/saghen/blink.cmp";
-              license = lib.licenses.mit;
-              maintainers = with lib.maintainers; [ redxtech ];
-            };
-          };
-
-          default = self'.packages.blink-cmp;
+        rustPlatform = pkgs.makeRustPlatform {
+          cargo = toolchain;
+          rustc = toolchain;
         };
 
-        # define the default dev environment
-        devenv.shells.default = {
-          name = "blink";
+        src = ./.;
+        version = "2024-08-02";
 
-          languages.rust = {
-            enable = true;
-            channel = "nightly";
+        # Build the Rust library used by the Vim plugin
+        blink-fuzzy-lib = rustPlatform.buildRustPackage {
+          pname = "blink-fuzzy-lib";
+          inherit src version;
+          cargoLock = {
+            lockFile = ./Cargo.lock;
+            outputHashes = {
+              "c-marshalling-0.2.0" = "sha256-eL6nkZOtuLLQ0r31X7uroUUDYZsWOJ9KNXl4NCVNRuw=";
+              "frizbee-0.1.0" = "sha256-9L3ZS7GMvLEqOBjC/VW2jHEZge/s6jRN7ok647Frl/M=";
+            };
           };
+        };
+      in {
+        blink-cmp = pkgs.vimUtils.buildVimPlugin {
+          pname = "blink-cmp";
+          inherit src version;
+          # Ensure the Rust library is available during the plugin build
+          buildInputs = [ blink-fuzzy-lib ];
+
+          postInstall = ''
+            mkdir -p ${out}/lib
+            cp ${blink-fuzzy-lib}/lib/libblink_cmp_fuzzy.so ${out}/lib/
+          '';
+
+          vimFiles.plugin = "plugin/blink-cmp.vim";
+
+          meta = {
+            description = "Performant, batteries-included completion plugin for Neovim";
+            homepage = "https://github.com/saghen/blink.cmp";
+            license = lib.licenses.mit;
+            maintainers = with lib.maintainers; [ redxtech ];
+          };
+        };
+
+        default = self'.packages.blink-cmp;
+      };
+
+      # define the default dev environment
+      devenv.shells.default = {
+        name = "blink";
+
+        languages.rust = {
+          enable = true;
+          channel = "nightly";
         };
       };
     };
+  };
 }
